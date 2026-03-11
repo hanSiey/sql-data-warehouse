946aef394ce886b51caeae0bf3dd488ba053cdcf
Hantsi Taumang <63910830+hanSiey@users.noreply.github.com>
2026-03-11 15:01:47 +0200

    Create initial tables for DataWarehouse
    This script creates multiple tables for customer, product, and sales information in the DataWarehouse database.

---
+++ scripts/ddl_create_tables.sql
@@ -0,0 +1,54 @@
+/*
+Create Tables from datasets
+*/
+use DataWarehouse;
+
+CREATE TABLE bronze.crm_cust_info (
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
+cst_create_date DATE
+);
+
+CREATE TABLE bronze.crm_prod_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
+);
+
+CREATE TABLE bronze.crm_sales_info(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt DATETIME,
+sls_ship_dt DATETIME,
+sls_due_dt DATETIME,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
+
+CREATE TABLE bronze.erp_cust_az12(
+CID NVARCHAR(50),
+BDATE DATETIME,
+GEN NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_loc_a101(
+CID NVARCHAR(50),
+CNTRY NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_px_cat_g1v2(
+ID NVARCHAR(50),
+CAT NVARCHAR(50),
+SUBCAT NVARCHAR(50),
+MAINTENANCE NVARCHAR(50) 
+);

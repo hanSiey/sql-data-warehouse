66fd40036506c28561ec638dedf32dde84c5f7b1
Hantsi Taumang <63910830+hanSiey@users.noreply.github.com>
2026-03-12 12:43:32 +0200

    Fix prd_key column definition in crm_prod_info table

--- scripts/silver_ddl_create_tables.sql
+++ scripts/silver_ddl_create_tables.sql
@@ -18,8 +18,8 @@ dwh_create_date DATETIME DEFAULT GETDATE()
 
 CREATE TABLE silver.crm_prod_info(
 prd_id INT,
-prd_key NVARCHAR(50),
 cat_id NVARCHAR(50),
+prd_key NVARCHAR(50),
 prd_nm NVARCHAR(50),
 prd_cost INT,
 prd_line NVARCHAR(50),

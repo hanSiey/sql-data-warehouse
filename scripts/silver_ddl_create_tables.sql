a4f5801b476128843fd86bd0d5e5274be18aae57
Hantsi Taumang <63910830+hanSiey@users.noreply.github.com>
2026-03-12 09:32:55 +0200

    Create tables in silver layer for data pipeline
    This SQL script creates multiple tables in the silver layer from bronze layer datasets, which are essential for further data transformations and analysis.

---
+++ scripts/silver_ddl_create_tables.sql
@@ -0,0 +1,62 @@
+/*
+Create Tables
+create tables in silver layer from bronze layer datasets. 
+This is the first step in the data pipeline to move data from bronze to silver layer. 
+The tables in silver layer will be used for further transformations and analysis.
+*/
+
+CREATE TABLE silver.crm_cust_info (
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
+cst_create_date DATE,
+dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE silver.crm_prod_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME,
+dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE silver.crm_sales_details(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT,
+dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE silver.erp_cust_az12(
+CID NVARCHAR(50),
+BDATE DATETIME,
+GEN NVARCHAR(50),
+dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE silver.erp_loc_a101(
+CID NVARCHAR(50),
+CNTRY NVARCHAR(50),
+dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE silver.erp_px_cat_g1v2(
+ID NVARCHAR(50),
+CAT NVARCHAR(50),
+SUBCAT NVARCHAR(50),
+MAINTENANCE NVARCHAR(50),
+dwh_create_date DATETIME DEFAULT GETDATE()
+);
